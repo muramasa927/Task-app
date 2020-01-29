a10bca99fa58de333b506e4938ed76a6e209c9b5
EC2 Default User <ec2-user@ip-172-31-35-192.ap-northeast-1.compute.internal>
2020-01-29 11:22:45 +0000

    finish A7

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-
-User.create!( name: "Sample User",
-              email: "sample@email.com",
-              password: "password",
-              password_confirmation: "password")
