ba58569fd0f26b244b0b8995aa19c60cacc41794
EC2 Default User <ec2-user@ip-172-31-35-192.ap-northeast-1.compute.internal>
2020-04-20 10:51:03 +0000

    20200420 Finish-Edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  # before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   def show
     @user = User.find(params[:id])
   end
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
 
   def new
+    if logged_in? && !current_user.admin?
+      flash[:info] = 'すでにログインしています。'
+      redirect_to current_user
+    end
     @user = User.new
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
@@ -19,9 +31,41 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #更新に成功した時の処理を記述
+      flash[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    #beforeフィルター
+    #ログイン済みのユーザーか確認する
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインして下さい"
+        redirect_to login_url
+      end
+    end
+    
+
 end
+
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+#confing:utf-8
+
+User.create!(name: "Sample User",
+             email: "sample@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+             
+30.times do |n|
+  name = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password)
+end
